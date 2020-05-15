9b19cbe8be1dbd542b82cbee185551d2e2b34a88
veytsberg <veytsberg.victoria@yandex.ru>
2020-05-15 00:03:32 -0700

    update server

--- server.py
+++ server.py
@@ -1,7 +1,8 @@
 import asyncio
+import re
 
 
-async def run_server(host, port):
+def run_server(host, port):
     loop = asyncio.get_event_loop()
     coro = loop.create_server(
         ClientServerProtocol,
@@ -19,13 +20,22 @@ async def run_server(host, port):
 
 class ClientServerProtocol(asyncio.Protocol):
     def connection_made(self, transport):
+        peername = transport.get_extra_info('peername')
+        print(f'Connection from {peername}')
         self.transport = transport
 
     def data_received(self, data):
-        asyncio.ensure_future(self.read, data)
-        # resp = process_data(data.decode())
-        # self.transport.write(resp.encode())
+        storage = {'key': 'value'}
+        resp = self.process_data(data.decode(), storage)
+        print(f'Data received: {resp}')
+        self.transport.write(b'ok')
+        print('Send: ok')
+        print(f'Send: {resp}')
 
-    async def read(self, data):
-        await data.decode()
+    @staticmethod
+    def process_data(data, storage):
+        if re.match(r'get ', data):
+            return storage['key']
+        return 'error\nwrong command\n\n'
 
+run_server('127.0.0.1', 3000)
